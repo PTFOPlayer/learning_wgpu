65a5236feea91dc15e0262f229d39cccead45fa9
Patryk <patryktofil03@gmail.com>
2024-09-18 09:21:58 +0200

    cleaning up code, implementing kernel selection

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+use std::io;
+
 use dot_product::dot_product::execute_dot_product;
 use saxpy::saxpy::execute_saxpy;
 use transpose::transpose::execute_transpose;
-use wgpu::RequestDeviceError;
+use wgpu::{Device, Queue, RequestAdapterOptions, RequestDeviceError};
 
-pub mod saxpy;
 pub mod dot_product;
 pub mod helpers;
+pub mod saxpy;
 pub mod transpose;
 
 #[derive(Debug)]
@@ -13,12 +15,68 @@ pub enum Error {
     AdapterAquasitionError,
     DeviceCreationError(RequestDeviceError),
     ExecutionError,
+    IoError,
+}
+
+impl From<io::Error> for Error {
+    fn from(_: io::Error) -> Self {
+        Error::IoError
+    }
 }
 
 fn main() -> Result<(), Error> {
-    smol::block_on(execute_saxpy())?;
-    smol::block_on(execute_dot_product())?;
-    smol::block_on(execute_transpose())?;
+    let (device, queue) = smol::block_on(init_device())?;
+
+    print!(
+        r#"
+Select shader:
+    (1) saxpy
+    (2) saxpy
+    (3) saxpy
+"#
+    );
+
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer)?;
+
+    match buffer[..buffer.len() - 1].parse::<u32>() {
+        Ok(1) => smol::block_on(execute_saxpy(device, queue))?,
+        Ok(2) => smol::block_on(execute_dot_product(device, queue))?,
+        Ok(3) => smol::block_on(execute_transpose(device, queue))?,
+        Ok(_) | Err(_) => {
+            println!("wrong input")
+        }
+    }
 
     Ok(())
+}
+
+/// init device
+/// Generates WGPU instance and aquires GPU
+pub async fn init_device() -> Result<(Device, Queue), Error> {
+    let instance = wgpu::Instance::default();
+
+    let adapter = match instance
+        .request_adapter(&RequestAdapterOptions::default())
+        .await
+    {
+        Some(adapter) => adapter,
+        None => return Err(Error::AdapterAquasitionError),
+    };
+
+    match adapter
+        .request_device(
+            &wgpu::DeviceDescriptor {
+                label: None,
+                required_features: wgpu::Features::empty(),
+                required_limits: wgpu::Limits::downlevel_defaults(),
+                memory_hints: wgpu::MemoryHints::MemoryUsage,
+            },
+            None,
+        )
+        .await
+    {
+        Ok(res) => Ok(res),
+        Err(error) => Err(Error::DeviceCreationError(error)),
+    }
 }
